6fd69b0e3ce4e74ea2355c136289e2017330bdfb
ishuks9 <ishuks9@gmail.com>
2021-07-08 17:22:12 +0530

    updated the UC4 file

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,18 +1,20 @@
 public class EmployeeWage{
-	public static void main(String args[]){	
-		System.out.println("Welcome to EmployeeWage");
+                public static final int is_Part_Time = 1;
+                public static final int is_Full_Time = 2;
+	public static void main(String args[]){
+		System.out.println("Welcome to EmpWage");
 		int wagePerhr = 20;
 		int empHrs = 0;
 		int dailyWage = 0;
-		int is_Part_Time = 1;
-		int is_Full_Time = 2;
-		double check = Math.floor(Math.random()*10)%3;
-		if(check == is_Full_Time){
-			empHrs = 8;
-		}else if(check == is_Part_Time){
+		int check =(int) Math.floor(Math.random()*10)%3;
+		switch(check){
+		case(is_Part_Time):
 			empHrs = 4;
-		}
-		else{
+			break;
+		case(is_Full_Time):
+			empHrs = 8;
+			break;
+		default :
 			empHrs = 0;
 		}
 		dailyWage = empHrs * wagePerhr;
