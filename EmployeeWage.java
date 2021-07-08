08d2313b02ff3fd1ed1392f2e80cd4e632bca3fb
ishuks9 <ishuks9@gmail.com>
2021-07-08 09:03:57 +0530

    updated the employee file

---
+++ EmployeeWage.java
@@ -0,0 +1,15 @@
+public class EmployeeWage{
+
+     public static void main (String args[]){
+      System.out.println("Welcome to employee wage");
+      int  isPresent = 1;
+       double check = Math.floor(Math.random()*10)%2;
+       if (check == isPresent ){
+        System.out.println("Employee is Prseent");
+       }
+      else 
+            System.out.println("Employee is Absent"); 
+
+      }
+
+          }
